3344500a899a7fb4113dae0799ad60fe987a611a
Vicente Matias <vicente.matias.g@gmail.com>
2022-08-09 16:55:53 -0300

    Cuarto commit - Query Methods

--- src/main/java/com/platzimarket/persistence/entity/Categoria.java
+++ src/main/java/com/platzimarket/persistence/entity/Categoria.java
@@ -11,11 +11,8 @@ public class Categoria {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column (name = "id_categoria")
     private Integer idCategoria;
-
     private String descripcion;
-
     private Boolean estado;
-
     @OneToMany(mappedBy = "categoria")
     private List<Producto> productos;
 
--- src/main/java/com/platzimarket/persistence/entity/Cliente.java
+++ src/main/java/com/platzimarket/persistence/entity/Cliente.java
@@ -8,17 +8,11 @@ import java.util.List;
 public class Cliente {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String nombre;
-
     private String apellido;
-
     private Integer celular;
-
     private String direccion;
-
     @Column(name = "correo_electronico")
     private String correoElectronico;
 
