7ccf9b0f08e805209104e857901ecb2608b7bc65
Vicente Matias <matias.vicente@branded.com.ar>
2023-09-21 17:44:07 -0300

    fix query method

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,6 +2,7 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,13 +10,14 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
     }
     public List<Producto> getByCategoria (int idCategoria) {
-        return productoCrudRepository.findByIdCategoriaOrderByAsc(idCategoria);
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
     }
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
